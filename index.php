d98dd55fe3941d989152a54541f32d20a81155f8
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2015-08-15 16:35:09 +0530

    Adding images and index page

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--<link rel="icon" href="../../favicon.ico">-->
+
+    <title>Mitra</title>
+
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+        <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color:#009688">
+			<div>
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#"><font family="Trebuchet MS, Helvetica, sans-serif;" size="18px" color="#FFF">mitra</font></a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<form class="navbar-form navbar-right">
+						<div class="form-group">
+						  <input type="text" placeholder="Email" class="form-control">
+						</div>
+						<div class="form-group">
+						  <input type="password" placeholder="Password" class="form-control">
+						</div>
+						<div class="form-group">
+							<button type="submit" class="btn btn-warning"><b>Sign in</b></button>
+						</div>
+						&nbsp;
+					</form>
+				</div><!--/.navbar-collapse -->
+			</div>
+		</nav>
+		<br/>
+		<br/>
+		<br/>
+		<div class="inner cover">
+            <h1 class="cover-heading">mitra</h1>
+            <p class="lead">Your one stop friend to all the employment problems.</p>
+            <p class="lead">
+				<a href="#" class="btn btn-lg btn-primary">Take a tour</a>
+				<a href="#" class="btn btn-lg btn-primary">Job listings</a>
+            </p>
+         </div>
+		<div class="mastfoot">
+			<div class="inner">    
+				<center>
+					<a href="#">Home</a> |  
+					<a href="#">Login</a> | 
+					<a href="#">Signup</a> | 
+					<a href="#">FAQs</a> | 
+					<a href="#">FAQs</a> |
+					<a href="#">Disclaimer</a> | 
+					<a href="#">Contact</a>
+					<br/><br/>
+					2015 &copy; Mitra
+				</center>				
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
